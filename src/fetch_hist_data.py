53fa8939563cd8a74c3a86dc54ce447b4bbc77fe
Tiaan Viviers <tiaanviv@gmail.com>
2024-11-23 19:59:25 +0200

    changed fetch lib

--- src/fetch_hist_data.py
+++ src/fetch_hist_data.py
@@ -1,32 +1,28 @@
-import yfinance as yf
+from dotenv import load_dotenv
+import os
+from alpha_vantage.foreignexchange import ForeignExchange
 
-def fetch_hist_data(symbol, start_date, end_date, interval="1d"):
-    """
-    Fetch historical price data for a given symbol using Yahoo Finance.
 
-    Args:
-        symbol (str): Ticker symbol (e.g., 'EURUSD=X' for Forex).
-        start_date (str): Start date in 'YYYY-MM-DD' format.
-        end_date (str): End date in 'YYYY-MM-DD' format.
-        interval (str): Data interval ('1d', '1h', '5m', etc.).
-
-    Returns:
-        pd.DataFrame: Dataframe containing price data.
-    """
-    print(f"Fetching data for {symbol} from {start_date} to {end_date}...")
-    data = yf.download(symbol, start=start_date, end=end_date, interval=interval)
+def fetch_alpha_vantage_data(api_key, from_symbol='EUR', to_symbol='USD'):
+    cc = ForeignExchange(key=api_key, output_format='pandas')
+    data, meta_data = cc.get_currency_exchange_daily(
+        from_symbol=from_symbol,
+        to_symbol=to_symbol,
+        outputsize='full'
+    )
+    data = data.sort_index()
+    data.rename(columns={
+        '1. open': 'Open',
+        '2. high': 'High',
+        '3. low': 'Low',
+        '4. close': 'Close'
+    }, inplace=True)
     return data
 
 if __name__ == "__main__":
-    # Params to fetch daily data
-    symbol = "EURUSD=X"
-    start_date = "2015-01-01"
-    end_date = "2024-11-23"
-    interval = "1d"
-
-    data = fetch_hist_data(symbol, start_date, end_date, interval)
-
-    # Save to CSV
-    output_file = "../data/raw/eur_usd_data.csv"
-    data.to_csv(output_file)
-    print(f"Data saved to {output_file}")
+    load_dotenv('../config/.env')
+    api_key = os.getenv("ALPHA_VANTAGE")
+    print(api_key)
+    data = fetch_alpha_vantage_data(api_key)
+    print(data.head())
+    data.to_csv('../data/raw/eur_usd_data.csv')
